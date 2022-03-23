f04f0421b504ed3372b0736a840e6d27b6a125d7
H. Bappi <53597251+hbappi@users.noreply.github.com>
2022-03-23 08:37:59 +0600

    H. Bappi

---
+++ Even-odd.kt
@@ -0,0 +1,12 @@
+fun main() {
+    //your number here
+    var number = 5;
+    
+    //calculate
+    if(number%2==0){
+        println("Given number is EVEN")
+    }else{
+        println("Given number in ODD")
+    }
+}
+
